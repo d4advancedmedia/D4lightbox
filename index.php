a2edfe803a1b1602c4b70b33831e54d529abb435
GrayofD4 <grayson@d4webdesign.com>
2015-08-19 14:44:02 -0700

    Ported in prettyPhoto ver. 3.1.6

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+/*
+	Plugin Name: D4 Lightbox
+	Plugin URI: https://github.com/d4advancedmedia/D4lightbox
+	GitHub Theme URI: https://github.com/d4advancedmedia/D4lightbox
+	GitHub Branch: master
+	Description: Responsive Image/Video Lightbox 
+	Version: 19Aug15
+	Author: D4 Adv. Media
+	License: GPL2
+*/
+
+
+?>
