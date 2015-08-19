338cbc811842b66e7c85bf2620f1664787010e93
GrayofD4 <grayson@d4webdesign.com>
2015-08-19 14:51:14 -0700

    made plugin.

--- index.php
+++ index.php
@@ -11,4 +11,27 @@
 */
 
 
+//	REGISTER - Javascripts & Styles
+	add_action('wp_enqueue_scripts', 'd4lightbox_scriptnstyle_enqueuer');
+	function d4lightbox_scriptnstyle_enqueuer() {
+			wp_register_script(
+				'd4lightbox-script', // slug
+				plugins_url( '/js/jquery.prettyPhoto.js' , __FILE__ ), // source file
+				array('jquery'), // dependencies
+				'3.1.6', // version
+				true // in footer?
+			);
+			wp_enqueue_script('d4lightbox-script');
+
+			wp_register_style(
+				'd4lightbox-style', // slug
+				plugins_url( '/css/style.css' , __FILE__ ), // source file
+				false, // dependencies
+				'3.1.6', // version
+				'screen' // media
+			);
+			wp_enqueue_style ('d4lightbox-style');
+			
+	}
+
 ?>
